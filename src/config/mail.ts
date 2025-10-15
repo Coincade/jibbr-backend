36144220285f735aa1b072ad9040377aa8f246f1
atharvadhumal <atharvaadhumal@gmail.com>
2025-10-15 11:38:51 +0530

    port changed for mail in mail.ts

--- src/config/mail.ts
+++ src/config/mail.ts
@@ -8,8 +8,8 @@ const createTransporter = () => {
 
   return nodemailer.createTransport({
     host: "smtp-relay.brevo.com",
-    port: 587,
-    secure: false,
+    port: 465,
+    secure: true,
     auth: {
       user: process.env.SMTP_USER,
       pass: process.env.SMTP_PASSWORD,
